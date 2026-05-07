a547a86a47bc223ca83b8065ed95823612ad950e
Xan1304 <2224802010194@student.tdmu.edu.vn>
2026-05-07 15:33:08 +0700

    v0.2.7

--- app/doc/export_doc.dart
+++ app/doc/export_doc.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() async {
   const name = "app";
-  const version = "1.1.6";
+  const version = "1.1.7";
   final libDir = Directory(Platform.script.resolve('../lib').toFilePath());
 
   if (!await libDir.exists()) {
